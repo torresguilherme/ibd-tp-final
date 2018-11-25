35820f9a65d0bc0c3e16e7c4f183d267dace4acb
Guilherme Torres <guilhermetorres97@gmail.com>
2018-11-25 19:48:42 -0200

    varias correçoes, distro-arch OK

--- gerador.py
+++ gerador.py
@@ -119,40 +119,63 @@ sobrenomes = [
 
 distros = [
         ("Manjaro", "Rolling", "pacman", "Manjaro Team", "Arch"),
-        ("Mint", "Common", "apt", "Mint Team", "Debian"),
-        ("elementary OS", "Common", "apt", "elementary Os Team", "Ubuntu"),
-        ("MX Linux", "Common", "apt", "community", "Debian/anitX"),
-        ("Ubuntu LTS", "LTS", "apt", "Canonical", "Debian"),
-        ("Ubuntu", "Common", "apt", "Canonical", "Debian"),
-        ("Debian Stable", "LTS", "apt", "community", "independent"),
-        ("Debian Testing", "Rolling", "apt", "community", "independent"),
-        ("Debian Unstable", "Rolling", "apt", "community", "independent"),
-        ("Fedora", "Rolling", "dnf", "Red Hat", "independent"),
-        ("openSUSE Leap", "LTS", "zypper", "SUSE", "independent"),
-        ("openSUSE Tumbleweed", "Rolling", "zypper", "SUSE", "independent"),
-        ("Solus", "Rolling", "eopkg", "Solus devs", "independent"),
-        ("Arch", "Rolling", "pacman", "community", "independent"),
-        ("CentOS", "LTS", "yum", "CentOS Project", "Red Hat Enterprise Linux"),
-        ("Red Hat Enterprise Linux", "LTS", "yum", "Red Hat", "independent")
+        ("Mint", "LTS", "apt", "Mint Team", "Debian Unstable"),
+        ("elementary OS", "Common", "apt", "elementary OS Team", "Ubuntu LTS"),
+        ("MX Linux", "Common", "apt", "community", "Debian Stable"),
+        ("Ubuntu LTS", "LTS", "apt", "Canonical", "Debian Stable"),
+        ("Ubuntu", "Common", "apt", "Canonical", "Debian Stable"),
+        ("Debian Stable", "LTS", "apt", "community", None),
+        ("Debian Testing", "Rolling", "apt", "community", None),
+        ("Debian Unstable", "Rolling", "apt", "community", None),
+        ("Fedora", "Rolling", "dnf", "Red Hat", None),
+        ("openSUSE Leap", "LTS", "zypper", "SUSE", None),
+        ("openSUSE Tumbleweed", "Rolling", "zypper", "SUSE", None),
+        ("Solus", "Rolling", "eopkg", "Solus devs", None),
+        ("Arch", "Rolling", "pacman", "community", None),
+        ("CentOS", "LTS", "yum", "Red Hat", "Red Hat Enterprise Linux"),
+        ("Red Hat Enterprise Linux", "LTS", "yum", "Red Hat", None)
 ]
 
+distro_arch = [
+    ("Manjaro", "x86_64"),
+    ("Mint", "i386"), ("Mint", "x86_64"),
+    ("elementary OS", "x86_64"),
+    ("MX Linux", "i686"), ("MX Linux", "x86_64"),
+    ("Ubuntu LTS", "armhf"), ("Ubuntu LTS", "i686"), ("Ubuntu LTS", "powerpc"), ("Ubuntu LTS", "ppc64el"), ("Ubuntu LTS", "s390x"), ("Ubuntu LTS", "x86_64"),
+    ("Ubuntu", "armhf"), ("Ubuntu", "i686"), ("Ubuntu", "powerpc"), ("Ubuntu", "ppc64el"), ("Ubuntu", "s390x"), ("Ubuntu", "x86_64"),
+    ("Debian Stable", "arm64"), ("Debian Stable", "armel"), ("Debian Stable", "armhf"), ("Debian Stable", "i686"), ("Debian Stable", "mips"), ("Debian Stable", "mipsel"), ("Debian Stable", "ppc64el"), ("Debian Stable", "s390x"), ("Debian Stable", "x86_64"),
+    ("Debian Testing", "arm64"), ("Debian Testing", "armel"), ("Debian Testing", "armhf"), ("Debian Testing", "i686"), ("Debian Testing", "mips"), ("Debian Testing", "mipsel"), ("Debian Testing", "ppc64el"), ("Debian Testing", "s390x"), ("Debian Testing", "x86_64"),
+    ("Debian Unstable", "arm64"), ("Debian Unstable", "armel"), ("Debian Unstable", "armhf"), ("Debian Unstable", "i686"), ("Debian Unstable", "mips"), ("Debian Unstable", "mipsel"), ("Debian Unstable", "ppc64el"), ("Debian Unstable", "s390x"), ("Debian Unstable", "x86_64"),
+    ("Fedora", "aarch64"), ("Fedora", "armhfp"), ("Fedora", "i686"), ("Fedora", "x86_64"),
+    ("openSUSE Leap", "arm64"), ("openSUSE Leap", "armhf"), ("openSUSE Leap", "i686"), ("openSUSE Leap", "ppc64"), ("openSUSE Leap", "ppc64el"), ("openSUSE Leap", "s390x"), ("openSUSE Leap", "x86_64"),
+    ("openSUSE Tumbleweed", "arm64"), ("openSUSE Tumbleweed", "armhf"), ("openSUSE Tumbleweed", "i686"), ("openSUSE Tumbleweed", "ppc64"), ("openSUSE Tumbleweed", "ppc64el"), ("openSUSE Tumbleweed", "s390x"), ("openSUSE Tumbleweed", "x86_64"),
+    ("Solus", "x86_64"),
+    ("Arch", "arm"), ("Arch", "x86_64"),
+    ("CentOS", "i386"), ("CentOS", "x86_64"),
+    ("Red Hat Enterprise Linux", "arm64"), ("Red Hat Enterprise Linux", "i386"), ("Red Hat Enterprise Linux", "ia64"), ("Red Hat Enterprise Linux", "IBM Z"), ("Red Hat Enterprise Linux", "ppc"), ("Red Hat Enterprise Linux", "ppc64el"), ("Red Hat Enterprise Linux", "s390"), ("Red Hat Enterprise Linux", "s390x"), ("Red Hat Enterprise Linux", "x86_64")   
+]
+
+distro_desktop = []
+
 desktops = [
-        ("AfterStep"),
-        ("Awesome"),
-        ("BlackBox"),
-        ("Cinnamon"),
-        ("GNOME"),
-        ("IceWM"),
-        ("KDE"),
-        ("MATE"),
-        ("LXQt"),
-        ("Pantheon"),
-        ("Xfce")
+        ("Cinnamon", "Nemo", "Xreader", "Xviewer", None),
+        ("GNOME", "Nautilus", "Evince", "Eye of GNOME", "GNOME Terminal"),
+        ("KDE", "Dolphin", "Okular", "Gwenview", "Konsole"),
+        ("MATE", "Caja", "Atril", "eom", "mate-terminal"),
+        ("LXQt", "PCManFM", None, "lximage-qt", "qterminal"),
+        ("Pantheon", "Pantheon Files", "Pantheon Reader", "Pantheon Photos", "Pantheon Terminal"),
+        ("Xfce", "Thunar", None, "Ristretto", "Xfce Terminal")
 ]
 
 maintainters = [
         ("community", "non-profit", "international", None),
-        ("Manjaro Team", "non-profit", "international", None)
+        ("Manjaro Team", "non-profit", "international", None),
+        ("Mint Team", "non-profit", "Ireland", None),
+        ("elementary OS Team", "non-profit", "USA", None),
+        ("Solus devs", "non-profit", "Ireland", None),
+        ("Canonical", "company", "Isle of Man", 125970000),
+        ("Red Hat", "company", "USA", 2900000000),
+        ("SUSE", "company", "Germany", 303400000)
 ]
 
 CHANCE_SOBRENOME_ADICIONAL = 0.4
@@ -188,8 +211,10 @@ DB_NAME = "ibd_final"
 #
 #
 #cursor = mydb.cursor()
-#sql_insert_query = """ INSERT INTO usuarios (nome, genero,idade, pais) 
+#sql_insert_query = """ INSERT INTO usuarios (id, nome, sobrenome, genero, idade, pais, usoProfissional) 
 #                       VALUES (%s,%s,%s,%s) """
+
+# USUARIO
 id = 0
 ages_normal = get_truncated_normal(mean=35, sd=20, low=MIN_AGE, upp=MAX_AGE)
 countries = list(dict(countries_for_language('en')).values())
@@ -201,7 +226,8 @@ for num_insertions in range(TOTAL_INSERT_AMOUNT):
     
     for buffer in range(INSERT_BUFFER_SIZE):
         line_tuple = (id,
-                      names[buffer],
+                      names[buffer].split(' ')[0],
+                      names[buffer].split(' ')[1],
                       generos[random.randint(0,len(generos) - 1)],
                       int(ages[buffer]),
                       countries[random.randint(0,len(countries) - 1)],
@@ -210,12 +236,31 @@ for num_insertions in range(TOTAL_INSERT_AMOUNT):
         id += 1
     print(insert_params)
 
+# DISTRO
 for it in distros:
     print(it)
 
+# USUARIO-DISTRO
 for num_insertions in range(TOTAL_INSERT_AMOUNT):
     insert_params = []
     for buffer in range(INSERT_BUFFER_SIZE):
         line_tuple = (random.randint(0, id), random.choice(distros)[0])
         insert_params.append(line_tuple)
     print(insert_params)
+
+# DESKTOP
+for it in desktops:
+    print(it)
+
+# MAINTAINER
+for it in maintainters:
+    print(it)
+
+# DISTRO-ARCH
+
+for it in distro_arch:
+    print(it)
+
+# DISTRO-DESKTOP
+for it in distro_desktop:
+    print(it)
